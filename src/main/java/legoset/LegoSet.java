1d70ee6c0d709f15c4b0f81ffb93420f770881c4
Kovács Nikolett <rue0103@gmail.com>
2020-04-01 11:41:06 +0200

    LegoSet.java és a Main elkészítése, az érdemi munka később történik.

--- src/main/java/legoset/LegoSet.java
+++ src/main/java/legoset/LegoSet.java
@@ -1,4 +1,15 @@
 package legoset;
 
+import java.util.List;
+
 public class LegoSet {
+    private String name;
+    private String theme;
+    private String subtheme;
+    private int year;
+    private int pieces;
+    private List<String> tags;
+    private List<Minifig> minifigs;
+    private double weight;
+    private String url;
 }
