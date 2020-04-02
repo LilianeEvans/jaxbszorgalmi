6556a0e180d9dd2f656f77a666a57b8a543c2fe4
Kovács Nikolett <rue0103@gmail.com>
2020-04-02 13:23:51 +0200

    Az érdemi munka elkészítése vagyis a Main, a Minifig és a Weight osztályok implementálása

--- src/main/java/legoset/LegoSet.java
+++ src/main/java/legoset/LegoSet.java
@@ -1,15 +1,38 @@
 package legoset;
 
-import java.util.List;
+import lombok.Data;
 
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.Year;
+import java.util.List;
+import java.util.Set;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"name","theme","subtheme","year","pieces","tags","minifigs", "weight", "url"})
+@Data
 public class LegoSet {
+
+    @XmlAttribute
+    private int number;
+
     private String name;
     private String theme;
     private String subtheme;
-    private int year;
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
+    private Year year;
     private int pieces;
-    private List<String> tags;
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name = "tag")
+    private Set<String> tags;
+
+    @XmlElementWrapper(name = "minifigs")
+    @XmlElement(name = "minifig")
     private List<Minifig> minifigs;
-    private double weight;
+
+    private Weight weight;
     private String url;
 }
